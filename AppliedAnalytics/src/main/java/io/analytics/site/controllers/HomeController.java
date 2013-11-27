6b3eef9f7465b6d8e1f665fb48fa5ba0cdb118a5
ariley2013 <u0530964@gmail.com>
2013-11-27 11:30:34 -0700

    Added Aspect functionality

--- AppliedAnalytics/src/main/java/io/analytics/site/controllers/HomeController.java
+++ AppliedAnalytics/src/main/java/io/analytics/site/controllers/HomeController.java
@@ -8,14 +8,16 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
+import org.springframework.web.servlet.ModelAndView;
 
 import com.google.api.client.auth.oauth2.Credential;
 
@@ -34,7 +36,6 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date);
