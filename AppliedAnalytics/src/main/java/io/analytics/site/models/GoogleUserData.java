f83259ba61500c68bfe034f008e38b7ba35857b4
Dave <invertedaerials@gmail.com>
2013-12-06 00:46:12 -0700

    Finished Google user connection - sample gives name, email, & picture.

---
+++ AppliedAnalytics/src/main/java/io/analytics/site/models/GoogleUserData.java
@@ -0,0 +1,45 @@
+package io.analytics.site.models;
+
+public class GoogleUserData {
+	private String id;
+	private String email;
+	private String verified_email;
+	private String name;
+	private String given_name;
+	private String family_name;
+	private String link;
+	private String picture;
+	private String gender;
+	private String locale;
+
+	public String getId() {
+		return id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getVerified_email() {
+		return verified_email;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getGiven_name() {
+		return given_name;
+	}
+	public String getFamily_name() {
+		return family_name;
+	}
+	public String getLink() {
+		return link;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public String getLocale() {
+		return locale;
+	}
+}
