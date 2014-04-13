da4743d7cb93679d17c6c36b224c14156c2668f3
GwenKnight <gwenknight2@gmail.com>
2014-04-12 21:32:35 -0600

    missed in last push

--- AppliedAnalytics/src/main/java/io/analytics/service/OverviewService.java
+++ AppliedAnalytics/src/main/java/io/analytics/service/OverviewService.java
@@ -110,56 +110,7 @@ public class OverviewService implements IOverviewService{
 
 			gaData = REPOSITORY.getOverviewDim(credential, profileID, startDate, endDate, 2, CoreReportingRepository.MEDIUM_DIMENSION);
 // TODO parse this data into json
-/*			if (gaData!=null) {
 
-				for (ColumnHeaders header : gaData.getColumnHeaders()) {
-					column++;
-					String name = header.getName();
-					if (name.equals("ga:newVisits"))
-						newVisitsColumn  = column;
-					if (name.equals("ga:percentNewVisits"))
-						percentNewVisitsColumn = column;
-					if (name.equals("ga:visits"))
-						visitsColumn = column;
-					if (name.equals("ga:visitBounceRate"))
-						visitBounceRateColumn = column;
-					if (name.equals("ga:pageviewsPerVisit"))
-						pageviewsPerVisitColumn = column;
-					if (name.equals("ga:avgTimeOnSite"))
-						avgTimeOnSiteColumn = column;
-
-					dataTypes.add(header.getDataType());
-				}
-
-				int newVisits = -1;
-				double percentNewVisits = -1.0;
-				int visits = -1;
-				double visitBounceRate = -1.0;
-				double pageviewsPerVisit = -1;
-				double avgTimeOnSite = -1.0;
-
-				List<List<String>> dataRows = gaData.getRows();
-				try {
-					for(List<String> row : dataRows) {
-						newVisits = Integer.parseInt(row.get(newVisitsColumn));
-						percentNewVisits = Double.parseDouble(row.get(percentNewVisitsColumn));
-						visits = Integer.parseInt(row.get(visitsColumn));
-						visitBounceRate = Double.parseDouble(row.get(visitBounceRateColumn));
-						pageviewsPerVisit = Integer.parseInt(row.get(pageviewsPerVisitColumn));
-						avgTimeOnSite = Double.parseDouble(row.get(avgTimeOnSiteColumn));
-					}
-				} catch (NumberFormatException e) {
-					//The metric we are retrieving is not numeric.
-					return null;
-				}
-
-				dataObject.setNewVisitsTotal(newVisits);
-				dataObject.setPercentNewVisitsTotal(percentNewVisits);
-				dataObject.setVisitsTotal(visits);
-				dataObject.setVisitBounceRateTotal(visitBounceRate);
-				dataObject.setPageviewsPerVisit(pageviewsPerVisit);
-				dataObject.setAvgTimePerVisit(avgTimeOnSite);
-*/
 			hasData = true;
 		}
 
