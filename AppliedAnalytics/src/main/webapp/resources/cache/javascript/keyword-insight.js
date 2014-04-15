abd88fa1276a33128119d587e5cf2f484b3bd26c
lynntesia <tesiagao@gmail.com>
2014-04-14 20:50:13 -0600

    scatter plot color coding

--- AppliedAnalytics/src/main/webapp/resources/cache/javascript/keyword-insight.js
+++ AppliedAnalytics/src/main/webapp/resources/cache/javascript/keyword-insight.js
@@ -77,10 +77,11 @@ function getKeywordInsightData(id, callback) {
 
 			sdata[i].values.push({
 				x: d.data.scatter.allBounceRate[i],
-				// y: d.data.scatter.allMultipageVisitsPercent[i],
-				y: d.data.scatter.allVisitsPercent[i],
-				size: .4,   //Configure the size of each scatter point
-				shape: "circle"  //Configure the shape of each scatter point.
+				y: d.data.scatter.allMultipageVisitsPercent[i],
+				//y: d.data.scatter.allVisitsPercent[i],
+				group: d.data.scatter.allGroups[i]
+				//size: .4,   //Configure the size of each scatter point
+				//shape: "circle"  //Configure the shape of each scatter point.
 			});
 		} 
 
