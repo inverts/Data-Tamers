aba6417434a068c70beb04682d03a69dbf6335c2
ariley2013 <u0530964@gmail.com>
2014-04-14 21:54:54 -0600

    Fixed Overview update issue with showing all views.
    Fixed a minor issue in page performance with the table height.

--- AppliedAnalytics/src/main/webapp/resources/cache/javascript/overview.js
+++ AppliedAnalytics/src/main/webapp/resources/cache/javascript/overview.js
@@ -117,6 +117,7 @@ function getOverviewData(id, callback) {
 //Updates the widget.
 function updateOverview(id) {
 	getOverviewData(id, function() {
+		$("#" + id + " .overviewVisual").hide();
 		$("#" + id + " .overviewVisual.active").show();
 	}); 
 }
